8056869c35bf80cd618ab766e581ca7f9bcfc060
ahmedramy <ahmedramy.e404@gmail.com>
2025-02-15 19:08:33 +0200

    feat: select branches selector

--- src/components/AddBranchesDialog.vue
+++ src/components/AddBranchesDialog.vue
@@ -3,7 +3,14 @@
     <header class="flex justify-start p-4">
       <div class="font-bold text-2xl">Add Branches</div>
     </header>
-    <div class="bg-gray-100 px-4 py-6"></div>
+    <div class="bg-gray-100 px-4 py-6">
+      <FSelect
+        label="Branches"
+        placeholder="Select branches..."
+        :options="branchOptions"
+        v-model="selectedValues"
+      />
+    </div>
     <footer class="flex justify-end gap-3 items-center p-4">
       <f-btn
         class="min-w-[5rem]"
@@ -26,10 +33,27 @@
 
 <script>
 import FBtn from "@/components/FBtn.vue";
+import FSelect from "@/components/FSelect.vue";
 
 export default {
   name: "AddBranchesDialog",
-  components: { FBtn },
+  components: { FSelect, FBtn },
+  data() {
+    return {
+      selectedValues: [],
+    };
+  },
+  computed: {
+    branchOptions() {
+      let branches = this.$store.getters["branches/branches"];
+      return branches.map((branch) => ({
+        label: `${branch.name}${
+          branch.reference ? ` (${branch.reference})` : ""
+        }`,
+        value: branch.id,
+      }));
+    },
+  },
 };
 </script>
 
